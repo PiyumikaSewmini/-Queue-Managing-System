122ee9e5fba9e10afdfa8c360726cd52db552ae5
PiyumikaSewmini <157671097+PiyumikaSewmini@users.noreply.github.com>
2024-04-27 13:43:49 +0530

    Add files via upload

---
+++ Class version/src/Customer.java
@@ -0,0 +1,11 @@
+class Customer {
+    String First_Name = "X";
+    String Last_Name = "X";
+    int Required_Burgers = 0;
+
+    void Adding_Customer(String firstname, String lastname, int count) {
+        First_Name = firstname;
+        Last_Name = lastname;
+        Required_Burgers = count;
+    }
+}
